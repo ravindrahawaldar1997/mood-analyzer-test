2ee53347601ea5dde3d241ae230c2d06a9c7547b
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-23 16:54:50 +0530

    Added: Program to throw null or empty mood custom exception

--- main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,23 +1,23 @@
 package com.bridgelabz.moodanalyzer;
 public class MoodAnalyzer {
-    private String message;
-    //Default Constructor
-    public MoodAnalyzer() {
-    }
+    public String message;
+
     //Parameterized Constructor
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    //Handle NULLPOINTER Exception using try-catch block
-    public String analyseMood() {
+    //Handle Customized Exception using try-catch block
+    public String analyseMood() throws MoodAnalysisException {
         try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
             if (message.contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
-        } catch(Exception e) {
-            return "HAPPY";
+        } catch(NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
         }
     }
 }

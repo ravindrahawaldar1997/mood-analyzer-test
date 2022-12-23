95a6dbe18566430bbbb97b1e1b9d054f5a1db541
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-23 17:28:43 +0530

    Added: program of given message of happy or sad by using constructor

--- main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,23 +1,21 @@
 package com.bridgelabz.moodanalyzer;
 public class MoodAnalyzer {
-    public String message;
+
+    private String message;
+
+    //Default Constructor
+    public MoodAnalyzer() {
+    }
 
     //Parameterized Constructor
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    //Handle Customized Exception using try-catch block
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message.length() == 0)
-                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
-            if (message.contains("sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch(NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
-        }
+    public String analyseMood(String iAmInHappyMood) {
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }

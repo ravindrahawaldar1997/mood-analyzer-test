e62c163630ff1b9c71fd5917c3e378c8d636d956
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-23 17:34:00 +0530

    Added: program for test case given message of happy or sad by using constructor

---
+++ test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void testMoodAnalysis_whenMoodIsSad() { //TC 1.1 "I am in sad mood"
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("I am in sad mood");
+        Assert.assertEquals(mood, "SAD");
+    }
+    @Test
+    public void testMoodAnalysis_whenMoodIsHappy() { //TC 1.2 I am in happy mood
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood1 = moodAnalyzer.analyseMood("I am in happy mood");
+        Assert.assertEquals(mood1, "HAPPY");
+    }
+}
